c8eacee3b0e3f19cae2198b7f72d5763280287a0
Cactus <115384394+Cactoidal@users.noreply.github.com>
2024-04-29 23:18:26 -0400

    Update FastCCIPEndpoint.sol

--- contracts/FastCCIPEndpoint.sol
+++ contracts/FastCCIPEndpoint.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.18;
 
 import {CCIPReceiver} from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
 import {Client} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import {Internal} from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Internal.sol";
 import {LinkTokenInterface} from "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 import {IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -32,25 +33,38 @@ contract FastCCIPEndpoint is CCIPReceiver {
 
     
     // Not compatible with tax-on-transfer tokens; could perhaps be toggled?
-    function fillOrder(Client.Any2EVMMessage calldata message) external {
+    function fillOrder(bytes calldata _message) external {
+
+        Internal.EVM2EVMMessage memory message = abi.decode(_message, (Internal.EVM2EVMMessage));
 
         bytes32 messageId = message.messageId;
         (address recipient, bytes memory data) = abi.decode(message.data, (address, bytes));
-        address token = message.destTokenAmounts[0].token;
+        address token = message.tokenAmounts[0].token;
 
         // The submitted amount must include the fee (0.1%)
-        uint amount = message.destTokenAmounts[0].amount - (message.destTokenAmounts[0].amount * FEE);
+        uint amount = message.tokenAmounts[0].amount - (message.tokenAmounts[0].amount * FEE);
        
         if (filledOrderPaths[messageId][recipient][token][amount][data] != address(0)) {
             revert OrderPathAlreadyFilled();
         }
         filledOrderPaths[messageId][recipient][token][amount][data] = msg.sender;
-        IERC20(token).transfer(recipient, amount);
+        IERC20(token).transferFrom(msg.sender, recipient, amount);
         emit OrderFilled(messageId);
         if (recipient.code.length == 0) {//|| !_recipient.supportsInterface(type(CCIPReceiver).interfaceId)) {
             return;
         }
-        CCIPReceiver(recipient).ccipReceive(message);
+
+    
+        Client.Any2EVMMessage memory ccipMessage = Client.Any2EVMMessage({
+            messageId: message.messageId,
+            sourceChainSelector: message.sourceChainSelector,
+            sender: abi.encode(message.sender),
+            data: abi.encode(message.data),
+            destTokenAmounts: message.tokenAmounts
+            });
+
+
+        CCIPReceiver(recipient).ccipReceive(ccipMessage);
     }
 
     // Right now only compatible with one token, due to the nature of order paths.
@@ -78,6 +92,21 @@ contract FastCCIPEndpoint is CCIPReceiver {
         }
             CCIPReceiver(recipient).ccipReceive(message);
         }
+
+    }
+
+
+    function filterOrder(bytes calldata _message, address _endpoint, address _token) public pure returns (address, uint256) {
+        Internal.EVM2EVMMessage memory message = abi.decode(_message, (Internal.EVM2EVMMessage));
+        address receiver = message.receiver;
+        address token = message.tokenAmounts[0].token;
+        if (receiver != _endpoint) {
+            return (address(0), 0);
+        }
+        if (token != _token) {
+            return (address(0), 0);
+        }
+        return (receiver, message.tokenAmounts[0].amount);
 
     }
 
